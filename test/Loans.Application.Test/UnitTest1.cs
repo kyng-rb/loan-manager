ae9b95d47f5911fa393d43ac884a8147660e0c3b
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2022-03-05 21:34:06 -0600

    Updated test's project name

--- test/Loans.Application.Test/UnitTest1.cs
+++ test/Loans.Application.Test/UnitTest1.cs
@@ -1,12 +1,15 @@
 using Xunit;
+using System;
 
 namespace Loans.Application.Test;
 
 public class UnitTest1
 {
+    private const string _test = "Test on Application";
+
     [Fact]
     public void Test1()
     {
-
+        Console.WriteLine(_test);
     }
 }
